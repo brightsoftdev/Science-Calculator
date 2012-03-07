70413a4d681d42dc4a04c7fe3fb25e0557c647b5
Richard Dong <richarddzh@163.com>
2012-03-07 16:02:25 +0800

    add Settings bundle

--- ScienceCalculator/DZMenuViewController.m
+++ ScienceCalculator/DZMenuViewController.m
@@ -12,6 +12,10 @@
     [[NSBundle mainBundle] \
     localizedStringForKey:(key) value:@"" table:nil]
 
+@interface DZMenuViewController ()
+- (void)userDefaultsChanged:(NSNotification *)notification;
+@end
+
 @implementation DZMenuViewController
 
 @synthesize forcedScientificNotation;
@@ -21,7 +25,7 @@
 {
     self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
     if (self != nil) {
-        NSLog(@"recreate");
+        //NSLog(@"recreate");
         self.title = LS(@"Menu");
         self.advancedMode = [[UISwitch alloc]initWithFrame:CGRectMake(0, 0, 79, 27)];
         [self.advancedMode addTarget:self 
@@ -31,8 +35,31 @@
         [self.forcedScientificNotation addTarget:self 
                               action:@selector(forcedScientificNotationChanged) 
                     forControlEvents:UIControlEventValueChanged];
+        [[NSNotificationCenter defaultCenter]
+         addObserver:self
+         selector:@selector(userDefaultsChanged:)
+         name:NSUserDefaultsDidChangeNotification
+         object:nil];
     }
     return self;
+}
+
+- (void)dealloc
+{
+    [[NSNotificationCenter defaultCenter]
+     removeObserver:self];
+}
+
+- (void)userDefaultsChanged:(NSNotification *)notification
+{
+    self.forcedScientificNotation.on = [[NSUserDefaults standardUserDefaults]boolForKey:@"scientific_notation"];
+    self.advancedMode.on = [[NSUserDefaults standardUserDefaults]boolForKey:@"advanced_mode"];
+}
+
+- (void)viewWillAppear:(BOOL)animated
+{
+    [super viewWillAppear:animated];
+    [self userDefaultsChanged:nil];
 }
 
 - (void)createModel
@@ -52,12 +79,16 @@
 
 - (void)advancedModeChanged
 {
-    NSLog(@"Advanced mode changed to: %@", self.advancedMode);
+    [[NSUserDefaults standardUserDefaults]
+     setBool:self.advancedMode.on 
+     forKey:@"advanced_mode"];
 }
 
 - (void)forcedScientificNotationChanged 
 {
-    NSLog(@"Forced scientific notation changed to: %d", self.forcedScientificNotation.on);
+    [[NSUserDefaults standardUserDefaults]
+     setBool:self.forcedScientificNotation.on 
+     forKey:@"scientific_notation"];
 }
 
 @end
