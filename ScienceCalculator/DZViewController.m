800c8b5f3410d2d5dd6186be077dab0d41cf6116
Richard Dong <richarddzh@163.com>
2012-04-07 09:19:12 +0800

    画完第一个keypad

--- ScienceCalculator/DZViewController.m
+++ ScienceCalculator/DZViewController.m
@@ -11,64 +11,6 @@
 #import "DZClassicCalculator.h"
 
 #pragma mark -
-#pragma mark kButton consts
-
-const int kButton_shift = 0;
-const int kButton_hyp = 1;
-const int kButton_deg = 2;
-const int kButton_xpowery = 3;
-const int kButton_xreciprocal = 4;
-const int kButton_delete = 5;
-const int kButton_nCr = 6;
-const int kButton_log2 = 7;
-const int kButton_log10 = 8;
-const int kButton_ln = 9;
-const int kButton_xsquareroot = 10;
-const int kButton_xsquare = 11;
-const int kButton_nfactorial = 12;
-const int kButton_sin = 13;
-const int kButton_cos = 14;
-const int kButton_tan = 15;
-const int kButton_leftpar = 16;
-const int kButton_rightpar = 17;
-const int kButton_pi = 18;
-const int kButton_7 = 19;
-const int kButton_8 = 20;
-const int kButton_9 = 21;
-const int kButton_neg = 22;
-const int kButton_C = 23;
-const int kButton_MR = 24;
-const int kButton_4 = 25;
-const int kButton_5 = 26;
-const int kButton_6 = 27;
-const int kButton_add = 28;
-const int kButton_sub = 29;
-const int kButton_Madd = 30;
-const int kButton_1 = 31;
-const int kButton_2 = 32;
-const int kButton_3 = 33;
-const int kButton_mul = 34;
-const int kButton_div = 35;
-const int kButton_Msub = 36;
-const int kButton_timestenpowerx = 37;
-const int kButton_0 = 38;
-const int kButton_point = 39;
-const int kButton_equ = 40;
-
-#define ADDBUTTON(index,text,action,tag) \
-    [self addButtonAtIndex:index \
-    withText:text \
-    tag:tag \
-    backgroundIndex:kImage_button50x42 \
-    backgroundIndexPressed:kImage_button50x42pressed \
-    addTarget:self \
-    andAction:@selector(action)]
-#define SETBUTTONIMAGE(index,image) \
-    [[self.allButtons objectAtIndex:index] \
-    setImage:[self.imagePool imageAtIndex:image] \
-    forState:UIControlStateNormal]
-
-#pragma mark -
 #pragma mark Private extention
 
 @interface DZViewController ()
@@ -76,24 +18,24 @@ const int kButton_equ = 40;
 @property (nonatomic,retain) DZClassicCalculator * calculator;
 @property (nonatomic,retain) NSMutableArray * allButtons;
 @property (nonatomic,retain) DZImagePool * imagePool;
+@property (nonatomic,retain) UIView * btnView1;
+@property (nonatomic,retain) UIView * btnView2;
 
-- (void)buttonPressed:(id)sender;
-- (void)shiftButtonPressed:(id)sender;
-- (void)hypButtonPressed:(id)sender;
-- (void)degButtonPressed:(id)sender;
-- (void)digitButtonPressed:(id)sender;
-- (void)deleteButtonLongPressed:(id)sender;
-- (void)operatorButtonPressed:(id)sender;
-
-- (void)updateDisplay;
-
-- (void)addButtonAtIndex:(NSInteger)index
-                withText:(NSString *)text
-                     tag:(NSInteger)tag
-         backgroundIndex:(NSInteger)bkImage
-  backgroundIndexPressed:(NSInteger)pressedImage
-               addTarget:(id)target
-               andAction:(SEL)action;
+- (void)buildUpView;
+- (void)addButtonAt:(NSInteger)index
+                 to:(UIView *)parentView
+           position:(NSInteger)row :(NSInteger)column
+                   :(NSInteger)width :(NSInteger)height
+              title:(NSString *)text
+             ofFont:(NSString *)name :(NSInteger)size 
+                   :(UIColor *)color
+                tag:(NSInteger)tag
+             target:(id)target :(SEL)action;
+- (void)addButton:(NSInteger)viewIdx
+                 :(NSInteger)idx
+                 :(NSString *)text
+                 :(NSInteger)tag
+                 :(SEL)action;
 
 @end
 
@@ -105,6 +47,7 @@ const int kButton_equ = 40;
 @synthesize allButtons;
 @synthesize imagePool;
 @synthesize calculator;
+@synthesize btnView1,btnView2;
 
 #pragma mark -
 #pragma mark buttonPress actions
@@ -115,225 +58,96 @@ const int kButton_equ = 40;
     TTURLAction * action = [[TTURLAction actionWithURLPath:@"tt://menu"]applyAnimated:YES];
     [navigator openURLAction:action];
 }
-
-- (void)buttonPressed:(id)sender
-{
-    UIButton * btn = sender;
-    switch (btn.tag) {
-        case kButton_point:
-            [self.calculator pressPoint];
-            break;
-        case kButton_neg:
-            [self.calculator pressNeg];
-            break;
-        case kButton_timestenpowerx:
-            [self.calculator pressTimesTenPowerX];
-            break;
-        case kButton_delete:
-            [self.calculator pressDelete];
-            break;
-        case kButton_equ:
-            [self.calculator pressEqu];
-            break;
-        case kButton_leftpar:
-            [self.calculator pressLeftPar];
-            break;
-        case kButton_rightpar:
-            [self.calculator pressRightPar];
-            break;
-        case kButton_cos:
-            [self.calculator pressFunction:kFunction_cos];
-            break;
-        default:
-            NSLog(@"btn: %d", btn.tag);
-            return;
-    }
-    [self updateDisplay];
-}
-
-- (void)shiftButtonPressed:(id)sender
-{
-    if (self.shiftIsPressed) {
-        self.shiftIsPressed = NO;
-        SETBUTTONIMAGE(kButton_xpowery, kImage_xpowery);
-        SETBUTTONIMAGE(kButton_nCr, kImage_nCr);
-        SETBUTTONIMAGE(kButton_log2, kImage_log2);
-        SETBUTTONIMAGE(kButton_log10, kImage_log10);
-        SETBUTTONIMAGE(kButton_ln, kImage_ln);
-        SETBUTTONIMAGE(kButton_pi, kImage_pi);
-        SETBUTTONIMAGE(kButton_MR, kImage_MR);
-        if (self.hypIsPressed) {
-            SETBUTTONIMAGE(kButton_sin, kImage_sinh);
-            SETBUTTONIMAGE(kButton_cos, kImage_cosh);
-            SETBUTTONIMAGE(kButton_tan, kImage_tanh);
-        } else {
-            SETBUTTONIMAGE(kButton_sin, kImage_sin);
-            SETBUTTONIMAGE(kButton_cos, kImage_cos);
-            SETBUTTONIMAGE(kButton_tan, kImage_tan);
-        }
-    } else {
-        self.shiftIsPressed = YES;
-        SETBUTTONIMAGE(kButton_xpowery, kImage_xrooty);
-        SETBUTTONIMAGE(kButton_nCr, kImage_nPr);
-        SETBUTTONIMAGE(kButton_log2, kImage_2powerx);
-        SETBUTTONIMAGE(kButton_log10, kImage_10powerx);
-        SETBUTTONIMAGE(kButton_ln, kImage_exp);
-        SETBUTTONIMAGE(kButton_pi, kImage_e);
-        SETBUTTONIMAGE(kButton_MR, kImage_MC);
-        if (self.hypIsPressed) {
-            SETBUTTONIMAGE(kButton_sin, kImage_arcsinh);
-            SETBUTTONIMAGE(kButton_cos, kImage_arccosh);
-            SETBUTTONIMAGE(kButton_tan, kImage_arctanh);
-        } else {
-            SETBUTTONIMAGE(kButton_sin, kImage_arcsin);
-            SETBUTTONIMAGE(kButton_cos, kImage_arccos);
-            SETBUTTONIMAGE(kButton_tan, kImage_arctan);
-        }
-    }
-}
-
-- (void)hypButtonPressed:(id)sender
-{
-    if (self.hypIsPressed) {
-        self.hypIsPressed = NO;
-        if (self.shiftIsPressed) {
-            SETBUTTONIMAGE(kButton_sin, kImage_arcsin);
-            SETBUTTONIMAGE(kButton_cos, kImage_arccos);
-            SETBUTTONIMAGE(kButton_tan, kImage_arctan);
-        } else {
-            SETBUTTONIMAGE(kButton_sin, kImage_sin);
-            SETBUTTONIMAGE(kButton_cos, kImage_cos);
-            SETBUTTONIMAGE(kButton_tan, kImage_tan);
-        }
-    } else {
-        self.hypIsPressed = YES;
-        if (self.shiftIsPressed) {
-            SETBUTTONIMAGE(kButton_sin, kImage_arcsinh);
-            SETBUTTONIMAGE(kButton_cos, kImage_arccosh);
-            SETBUTTONIMAGE(kButton_tan, kImage_arctanh);
-        } else {
-            SETBUTTONIMAGE(kButton_sin, kImage_sinh);
-            SETBUTTONIMAGE(kButton_cos, kImage_cosh);
-            SETBUTTONIMAGE(kButton_tan, kImage_tanh);
-        }
-    }
-}
-
-- (void)degButtonPressed:(id)sender
-{
-    if (self.degIsPressed) {
-        self.degIsPressed = NO;
-        self.ledDegRad.image = [self.imagePool imageAtIndex:kImage_LEDrad];
-    } else {
-        self.degIsPressed = YES;
-        self.ledDegRad.image = [self.imagePool imageAtIndex:kImage_LEDdeg];
-    }
-}
-
-- (void)digitButtonPressed:(id)sender
-{
-    switch ([sender tag]) {
-        case kButton_0:
-            [self.calculator pressDigit:0];
-            break;
-        case kButton_1:
-            [self.calculator pressDigit:1];
-            break;
-        case kButton_2:
-            [self.calculator pressDigit:2];
-            break;
-        case kButton_3:
-            [self.calculator pressDigit:3];
-            break;
-        case kButton_4:
-            [self.calculator pressDigit:4];
-            break;
-        case kButton_5:
-            [self.calculator pressDigit:5];
-            break;
-        case kButton_6:
-            [self.calculator pressDigit:6];
-            break;
-        case kButton_7:
-            [self.calculator pressDigit:7];
-            break;
-        case kButton_8:
-            [self.calculator pressDigit:8];
-            break;
-        case kButton_9:
-            [self.calculator pressDigit:9];
-            break;
-    }
-    [self updateDisplay];
-}
-
-- (void)deleteButtonLongPressed:(id)sender
-{
-    UILongPressGestureRecognizer * gr = sender;
-    if (gr.state == UIGestureRecognizerStateEnded) {
-        [self.calculator longPressDelete];
-        [self updateDisplay];
-    }
-}
-
-- (void)operatorButtonPressed:(id)sender
-{
-    switch ([sender tag]) {
-        case kButton_add:
-            [self.calculator pressOperator:kOperator_add];
-            break;
-        case kButton_sub:
-            [self.calculator pressOperator:kOperator_sub];
-            break;
-        case kButton_mul:
-            [self.calculator pressOperator:kOperator_mul];
-            break;
-        case kButton_div:
-            [self.calculator pressOperator:kOperator_div];
-            break;
-        case kButton_xpowery:
-            [self.calculator pressOperator:
-             (self.shiftIsPressed?kOperator_root:kOperator_power)];
-            break;
-        case kButton_nCr:
-            [self.calculator pressOperator:
-             (self.shiftIsPressed?kOperator_nPr:kOperator_nCr)];
-            break;
-    }
-    [self updateDisplay];
-}
-
+                 
 #pragma mark -
 #pragma mark Utilities
 
-- (void)updateDisplay
+#pragma mark -
+#pragma mark add buttons and build views
+
+- (void)buildUpView
 {
-    self.numberLabel.text = [self.calculator displayNumber];
-    self.exprLabel.text = [self.calculator displayExpression];
+    self.btnView1 = [[UIView alloc]
+                     initWithFrame:CGRectMake(0, 124, 320, 336)];
+    [self addButton:1:0:@"Fn":0:@selector(menuButtonPressed:)];
+    [self addButton:1:1:@"M+":1:@selector(menuButtonPressed:)];
+    [self addButton:1:2:@"M−":1:@selector(menuButtonPressed:)];
+    [self addButton:1:3:@"MR":1:@selector(menuButtonPressed:)];
+    [self addButton:1:4:@"MC":1:@selector(menuButtonPressed:)];
+    [self addButton:1:5:@"(":1:@selector(menuButtonPressed:)];
+    [self addButton:1:6:@")":1:@selector(menuButtonPressed:)];
+    [self addButton:1:7:@"C":1:@selector(menuButtonPressed:)];
+    [self addButton:1:8:@"CE":1:@selector(menuButtonPressed:)];
+    [self addButton:1:9:@"DEL":1:@selector(menuButtonPressed:)];
+    [self addButton:1:10:@"÷":1:@selector(menuButtonPressed:)];
+    [self addButton:1:11:@"7":1:@selector(menuButtonPressed:)];
+    [self addButton:1:12:@"8":1:@selector(menuButtonPressed:)];
+    [self addButton:1:13:@"9":1:@selector(menuButtonPressed:)];
+    [self addButton:1:14:@"×10ⁿ":1:@selector(menuButtonPressed:)];
+    [self addButton:1:15:@"×":1:@selector(menuButtonPressed:)];
+    [self addButton:1:16:@"4":1:@selector(menuButtonPressed:)];
+    [self addButton:1:17:@"5":1:@selector(menuButtonPressed:)];
+    [self addButton:1:18:@"6":1:@selector(menuButtonPressed:)];
+    [self addButton:1:19:@"±":1:@selector(menuButtonPressed:)];
+    [self addButton:1:20:@"−":1:@selector(menuButtonPressed:)];
+    [self addButton:1:21:@"1":1:@selector(menuButtonPressed:)];
+    [self addButton:1:22:@"2":1:@selector(menuButtonPressed:)];
+    [self addButton:1:23:@"3":1:@selector(menuButtonPressed:)];
+    [self addButton:1:24:@"F-E":1:@selector(menuButtonPressed:)];
+    [self addButton:1:25:@"+":1:@selector(menuButtonPressed:)];
+    [self addButton:1:26:@"0":1:@selector(menuButtonPressed:)];
+    [self addButton:1:27:@".":1:@selector(menuButtonPressed:)];
+    [self addButton:1:28:@"ans":1:@selector(menuButtonPressed:)];
+    [self addButton:1:29:@"=":1:@selector(menuButtonPressed:)];
+    self.btnView2 = [[UIView alloc]
+                     initWithFrame:CGRectMake(0, 124, 320, 336)];
 }
 
-#pragma mark -
-#pragma mark add and move buttons
-
-- (void)addButtonAtIndex:(NSInteger)index
-                withText:(NSString *)text
-                     tag:(NSInteger)tag
-         backgroundIndex:(NSInteger)bkImage
-  backgroundIndexPressed:(NSInteger)pressedImage
-               addTarget:(id)target
-               andAction:(SEL)action
+- (void)addButtonAt:(NSInteger)index
+                 to:(UIView *)parentView
+           position:(NSInteger)row :(NSInteger)column
+                   :(NSInteger)width :(NSInteger)height
+              title:(NSString *)text
+             ofFont:(NSString *)name :(NSInteger)size 
+                   :(UIColor *)color
+                tag:(NSInteger)tag
+             target:(id)target :(SEL)action
 {
     UIButton * btn = [UIButton buttonWithType:UIButtonTypeCustom];
     [btn setTag:tag];
-    [btn setBackgroundImage:[self.imagePool imageAtIndex:bkImage]
+    [btn setBackgroundImage:
+     [self.imagePool imageAtIndex:kImage_button]
                    forState:UIControlStateNormal];
-    [btn setBackgroundImage:[self.imagePool imageAtIndex:pressedImage]
+    [btn setBackgroundImage:
+     [self.imagePool imageAtIndex:kImage_buttonPressed]
                    forState:UIControlStateHighlighted];
     [btn setTitle:text forState:UIControlStateNormal];
+    [btn setTitleColor:color forState:UIControlStateNormal];
+    btn.titleEdgeInsets = UIEdgeInsetsMake(0, 0, 6, 6);
+    btn.titleLabel.font = [UIFont fontWithName:name 
+                                          size:size];
+    btn.titleLabel.adjustsFontSizeToFitWidth = YES;
     [btn addTarget:target action:action
   forControlEvents:UIControlEventTouchUpInside];
     [self.allButtons insertObject:btn atIndex:index];
-    [self.view addSubview:btn];
+    [parentView addSubview:btn];
+    [btn setFrame:CGRectMake(64*column-64,56*row-56,
+                             64*width,56*height)];
+}
+
+- (void)addButton:(NSInteger)viewIdx 
+                 :(NSInteger)idx 
+                 :(NSString *)text 
+                 :(NSInteger)tag 
+                 :(SEL)action
+{
+    [self addButtonAt:idx
+                   to:(viewIdx==1)?(self.btnView1):(self.btnView2)
+             position:(idx%30)/5+1 :(idx%30)%5+1 :1 :1
+                title:text
+               ofFont:@"Helvetica-Bold" :32 :[UIColor whiteColor]
+                  tag:tag 
+               target:self :action];
 }
 
 #pragma mark -
@@ -343,66 +157,11 @@ const int kButton_equ = 40;
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
-    self.longPressRecognizer = 
-        [[UILongPressGestureRecognizer alloc]
-         initWithTarget:self 
-         action:@selector(deleteButtonLongPressed:)];
     self.calculator = [DZClassicCalculator sharedCalculator];
-    self.numberLabel.text = [calculator displayNumber];
-    self.exprLabel.text = [calculator displayExpression];
-    self.numberLabel.adjustsFontSizeToFitWidth = YES;
-    //self.exprLabel.adjustsFontSizeToFitWidth = YES;
-    self.exprLabel.lineBreakMode = UILineBreakModeHeadTruncation;
-    self.exprLabel.textAlignment = UITextAlignmentRight;
-    self.shiftIsPressed = NO;
-    self.hypIsPressed = NO;
-    self.degIsPressed = YES;
-    self.ledM.image = nil;
     self.imagePool = [DZImagePool new];
     self.allButtons = [NSMutableArray array];
-    ADDBUTTON(0,shift,shiftButtonPressed:);
-    ADDBUTTON(1,hyp,hypButtonPressed:);
-    ADDBUTTON(2,deg,degButtonPressed:);
-    ADDBUTTON(3,xpowery,operatorButtonPressed:);
-    ADDBUTTON(4,xreciprocal,buttonPressed:);
-    ADDBUTTON(5,delete,buttonPressed:);
-    ADDBUTTON(6,nCr,operatorButtonPressed:);
-    ADDBUTTON(7,log2,buttonPressed:);
-    ADDBUTTON(8,log10,buttonPressed:);
-    ADDBUTTON(9,ln,buttonPressed:);
-    ADDBUTTON(10,xsquareroot,buttonPressed:);
-    ADDBUTTON(11,xsquare,buttonPressed:);
-    ADDBUTTON(12,nfactorial,buttonPressed:);
-    ADDBUTTON(13,sin,buttonPressed:);
-    ADDBUTTON(14,cos,buttonPressed:);
-    ADDBUTTON(15,tan,buttonPressed:);
-    ADDBUTTON(16,leftpar,buttonPressed:);
-    ADDBUTTON(17,rightpar,buttonPressed:);
-    ADDBUTTON(18,pi,buttonPressed:);
-    ADDBUTTON(19,7,digitButtonPressed:);
-    ADDBUTTON(20,8,digitButtonPressed:);
-    ADDBUTTON(21,9,digitButtonPressed:);
-    ADDBUTTON(22,neg,buttonPressed:);
-    ADDBUTTON(23,C,buttonPressed:);
-    ADDBUTTON(24,MR,buttonPressed:);
-    ADDBUTTON(25,4,digitButtonPressed:);
-    ADDBUTTON(26,5,digitButtonPressed:);
-    ADDBUTTON(27,6,digitButtonPressed:);
-    ADDBUTTON(28,add,operatorButtonPressed:);
-    ADDBUTTON(29,sub,operatorButtonPressed:);
-    ADDBUTTON(30,Madd,buttonPressed:);
-    ADDBUTTON(31,1,digitButtonPressed:);
-    ADDBUTTON(32,2,digitButtonPressed:);
-    ADDBUTTON(33,3,digitButtonPressed:);
-    ADDBUTTON(34,mul,operatorButtonPressed:);
-    ADDBUTTON(35,div,operatorButtonPressed:);
-    ADDBUTTON(36,Msub,buttonPressed:);
-    ADDBUTTON(37,timestenpowerx,buttonPressed:);
-    ADDBUTTON(38,0,digitButtonPressed:);
-    ADDBUTTON(39,point,buttonPressed:);
-    ADDBUTTON2(40,equ,buttonPressed:);
-    [[self.allButtons objectAtIndex:kButton_delete]addGestureRecognizer:self.longPressRecognizer];
-    [self moveAllButtonsToDeviceOrientationAnimated:NO];
+    [self buildUpView];
+    [self.view addSubview:btnView1];
 }
 
 - (void)viewDidUnload
